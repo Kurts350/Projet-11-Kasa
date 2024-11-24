356b5167b01ee2f89f6b3f62eb8fe3e9b918fa79
Biaguy Niakate <n.biaguy@gmail.com>
2024-11-24 21:27:13 +0100

    finalisation du footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "./components/Navbar.jsx";
 import Main from "./components/Main.jsx";
 import Banner from "./components/Banner.jsx";
+import Footer from "./components/Footer.jsx";
 import ApartmentSection from "./components/ApartmentSection.jsx";
 import './sass/App.scss'
 
@@ -13,6 +14,7 @@ function App() {
         <Banner />
         <ApartmentSection />
       </Main>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "../sass/Footer.scss";
+import logo from "../assets/logo-footer.png";
+function Footer() {
+  return (
+    <div className="footer">
+        <div className="footer__logo">
+          <img src={logo} alt="logo" />
+        </div>
+        <div className="footer__copyright"> © 2020 Kasa. All rights reserved</div>
+    </div>
+  );
+}
+
+export default Footer;
