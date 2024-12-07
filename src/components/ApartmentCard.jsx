d011ea12f30d7f7fd42feae2529860f010a66b23
Biaguy Niakate <n.biaguy@gmail.com>
2024-12-07 12:12:34 +0100

    affichage dynamique des cover sur la home page

--- src/components/ApartmentCard.jsx
+++ src/components/ApartmentCard.jsx
@@ -2,16 +2,16 @@ import React from "react";
 import "../sass/components/ApartmentCard.scss";
 import { NavLink } from "react-router-dom";
 
-function ApartmentCard() {
+function ApartmentCard(props) {
+  console.log("props dans la card : ", props);
   return (
     <NavLink to="/apartment">
       <div className="apartment-card">
-        <div className="apartment-card__title">
-          Titre de la <br /> location
-        </div>
+        <img src={props.imageUrl} alt="" />
+        <div className="apartment-card__title">{props.title}</div>
       </div>
     </NavLink>
   );
 }
 
-export {ApartmentCard};
+export { ApartmentCard };
--- src/components/ApartmentSection.jsx
+++ src/components/ApartmentSection.jsx
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "../sass/components/ApartmentSection.scss";
-import {ApartmentCard} from "./ApartmentCard.jsx";
+import { ApartmentCard } from "./ApartmentCard.jsx";
+import { useState } from "react";
 
 function ApartmentSection() {
+  const [apartments, setApartments] = useState([]);
+  useEffect(fetchApartments, []);
+  function fetchApartments() {
+    fetch("db.json")
+      .then((res) => res.json())
+      .then((res) => setApartments(res))
+      .catch(console.error);
+  }
   return (
     <div className="grid">
-      <ApartmentCard />
-      <ApartmentCard />
-      <ApartmentCard />
-      <ApartmentCard />
+      {apartments.map((apartment) => (
+        <ApartmentCard 
+        title={apartment.title}
+        imageUrl={apartment.cover}/>
+      ))}
     </div>
   );
 }
