9e49c5bfc4811c6e653ff5d0223ad857ca34429f
Biaguy Niakate <n.biaguy@gmail.com>
2025-01-30 16:14:44 +0100

    correction de l'import router dans le fichier index.jsx

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
-import {router} from './routes/router.jsx';
+import {router} from './routes/router';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
